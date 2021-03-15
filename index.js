dda4368a5a84d4c56bccf41772cdf32f8f875f6d
alancphilpott <alanp1995@gmail.com>
2021-03-15 17:18:09 +0000

    Exercise 3.1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const contacts = require('./sampleData')[0].contacts
 
 app.use(express.json())
 
 app.get('/', (_, res) => {
   res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/contacts', (req, res) => {
+  res.json(contacts)
 })
 
 app.listen(3001, () => console.log(`Server Listening on Port 3001`))
---
+++ sampleData.js
@@ -0,0 +1,33 @@
+const contacts = [
+  {
+    contacts: [
+      {
+        name: 'Alan Philpott',
+        number: '(429) 304-2158',
+        id: 1
+      },
+      {
+        name: 'Arto Hellas',
+        number: '(499) 926-9005',
+        id: 2
+      },
+      {
+        name: 'Ada Lovelace',
+        number: '(229) 797-0796',
+        id: 3
+      },
+      {
+        name: 'Dan Abramov',
+        number: '(397) 845-7465',
+        id: 4
+      },
+      {
+        name: 'Mary Poppendieck',
+        number: '(728) 360-4209',
+        id: 5
+      }
+    ]
+  }
+]
+
+module.exports = contacts
